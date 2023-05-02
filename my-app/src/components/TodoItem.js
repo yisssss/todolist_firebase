3ba002eaa949f7c68a735b4c402acdf7feb1298a
yisssss <81618079+yisssss@users.noreply.github.com>
2023-05-02 13:30:49 +0900

    deadline date 추가

--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -23,6 +23,10 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       >
         {todo.text}
       </span>
+      <span className={styles.todoText}>{`${todo.deadline
+        .toDate()
+        .toString()
+        .substring(0, 15)}`}</span>
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
       <button onClick={onDelete}>Delete</button>
