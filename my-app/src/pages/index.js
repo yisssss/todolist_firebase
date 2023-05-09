dd12d3639c90883bbb56cc006e45ca03ccfcb839
yisssss <81618079+yisssss@users.noreply.github.com>
2023-05-09 11:31:57 +0900

    add login

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import TodoList from "../components/TodoList";
+import { useRouter } from " next/router";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace("/auth/signin");
+    },
+  });
   return (
     <div>
       <TodoList />
